f2083f3b0729b593854c034184804b5505cf36fe
Matias Fiordelli <>
2023-06-28 16:45:12 -0300

    .

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ app.use(cors())
 const port = process.env.port
 const mainProducts = await GetData()
 
-app.options("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
-  });
-
 app.get('/', (req, res)=>{
     res.send('Welcome')
 }) 
