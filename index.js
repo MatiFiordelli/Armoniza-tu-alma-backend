c0241a6318f2eb843d53750d7943a9d3d3e7eae0
Matias Fiordelli <>
2023-06-28 14:27:33 -0300

    .

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.use((req, res, next)=>{
         next();
       }
 })
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
 
 app.get('/main-products', cors({origin:'*'}), (req, res)=>{
     res.json(mainProducts)
