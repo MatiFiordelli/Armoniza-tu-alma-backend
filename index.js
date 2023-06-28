e31114f783e8d51fe6e55b87f4b8bf985a1bfe37
Matias Fiordelli <>
2023-06-28 16:34:57 -0300

    .

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import GetData from './mongoData.js'
 
 const app = express()
+app.use(cors())
 app.use(()=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -11,25 +12,6 @@ app.use(()=>{
 const port = process.env.port
 const mainProducts = await GetData()
 
-app.get('/', (req, res)=>{
-    res.set('Access-Control-Allow-Origin', '*')
-    res.send('Welcome')
-}) 
-
-app.use((req, res, next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    if ('OPTIONS' === req.method) {
-        //respond with 200
-        res.send(200);
-      }
-      else {
-      //move on
-        next();
-      }
-})
 app.options("/*", function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -37,14 +19,15 @@ app.options("/*", function(req, res, next){
     res.send(200);
   });
 
-app.get('/main-products', cors({origin:'*'}), (req, res)=>{
-    res.json(mainProducts)
-})
+app.get('/', (req, res)=>{
+    res.set('Access-Control-Allow-Origin', '*')
+    res.send('Welcome')
+}) 
 
-/* app.get('/main-products', (req, res)=>{
+app.get('/main-products', (req, res)=>{
     res.set('Access-Control-Allow-Origin', '*')
     res.json(mainProducts)
-}) */
+})
 
 app.listen(port || 3001, ()=>console.log('server online'))
 
